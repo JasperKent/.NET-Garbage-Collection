9fd43dd825518958d6c0be55a8f2c2f779a44702
Jasper Kent <jasper_kent@hotmail.com>
2021-03-19 08:36:32 +0000

    Add project files.

---
+++ GarbageCollection/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GarbageCollection
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public Person ChildOne { get; set; }
+        public Person ChildTwo { get; set; }
+
+        ~Person()
+        {
+            Console.WriteLine($"   Collecting {Name}.");
+        }
+    }
+}
---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GarbageCollection
+{
+    class Program
+    {
+        static void ShortLives (Person parent)
+        {
+            Person fred = new Person
+            {
+                Name = "Fred",
+                ChildOne = new Person { Name = "Bamm-Bamm" }
+            };
+
+            parent.ChildTwo = fred.ChildOne;
+        }
+
+        static void Run()
+        {
+            Person wilma = new Person 
+            { 
+                Name = "Wilma",
+                ChildOne = new Person { Name = "Pebbles"}
+            };
+
+            ShortLives(wilma);
+
+            Console.WriteLine("Leaving 'ShortLives'...");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        static void Main()
+        {
+            Run();
+
+            Console.WriteLine("\nLeaving 'Run'...");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
